7f787357fd3072cbb2eed748ae9daae072e5868b
MamathaBerragari <166118740+MamathaBerragari@users.noreply.github.com>
2026-05-13 11:52:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
   "http://127.0.0.1:5175",
-  "http://niharika-s3-serverless.s3-website-us-east-1.amazonaws.com"
+  "http://foodway-frontend.s3-website.ap-south-1.amazonaws.com"
 ];
 
 // ✅ CORS Middleware
