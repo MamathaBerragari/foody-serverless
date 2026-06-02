56bfc9a96a4a889c822cd09a1f37e4a95317a678
MamathaBerragari <166118740+MamathaBerragari@users.noreply.github.com>
2026-06-02 18:47:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
   "http://127.0.0.1:5175",
-  "http://50.17.105.38"
+  "http://98.95.238.85"
 ];
 
 // ✅ CORS Middleware
